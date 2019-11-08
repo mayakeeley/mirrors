269e915da476875101f8bdbdef2372ad75602511
Maya Keeley <maya.keeley@Nology.local>
2019-11-08 11:46:50 +0000

    added onclick

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 
 class Image extends Component {
-  state = {};
+  state = {
+    assignedInvert: this.props.percentage,
+    currentInvert: this.props.percentage
+  };
+
+  switchInvert = () => {
+    this.state.currentInvert === this.state.assignedInvert
+      ? this.setState({ currentInvert: 0 })
+      : this.setState({ currentInvert: this.state.assignedInvert });
+  };
+
   render() {
     return (
       <img
         src={this.props.photo}
         alt={this.props.name}
-        style={{ filter: `invert(${this.props.percentage}%)`, height: "200px" }}
-        key={this.props.percentage}
+        style={{
+          filter: `invert(${this.state.currentInvert}%)`,
+          height: "200px",
+          transition: "1s filter"
+        }}
+        onClick={this.switchInvert}
       />
     );
   }
